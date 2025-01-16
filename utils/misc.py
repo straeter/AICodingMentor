b90fa0601b14409dc21db4277564010c9d9ead9e
straeter <mail@christoph-straeter.com>
2025-01-16 09:47:57 +0100

    remove obsolete code 2

--- utils/misc.py
+++
@@ -1,7 +0,0 @@
-import secrets
-import string
-
-
-def generate_unique_id(length=16):
-    characters = string.ascii_letters  # Uppercase and lowercase letters
-    return ''.join(secrets.choice(characters) for _ in range(length))
